6dd04aca85b64205da54d4c711403da0d0c96665
Nick Yennerell <njyennerell@wm.edu>
2026-09-02 16:21:14 -0400

    Update 1N

--- API_Assignment.py
+++ API_Assignment.py
@@ -1,8 +1,41 @@
 # Team 6
 # Michael Garbarino, Nick Yennerell, Sadie McMahon
 
+# Documentation
+## Question/Story
+## Data choices
+## Interactivity
+## Takeaway
+## Group Evaluation
+
+# Importing Packages
+from dash import Dash, dcc, html, Input,Output
+import yfinance as yf
+import pandas as pd
+
+# Get Data from Yahoo Finance
+
+tickers = [" ", " "]
+
+data = yf.download(tickers,
+    period="##y",
+    interval="##d",
+    auto_adjust=True)
+
+#Closing Prices Only
+prices = data["Close"].dropna()
+
 
 # Dash App
+app = Dash(__name__)
+app.title = "####"
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
 ## Fetch API
 ### 2 series over 1 year
 ## Display a static image (logo, graphic, image)
@@ -12,9 +45,3 @@
 ### Two analytics: normalization, percent change, rolling average, resampling
 # Labels and Source
 
-# Documentation
-## Question/Story
-## Data choices
-## Interactivity
-## Takeaway
-## Group Evaluation
