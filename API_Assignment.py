474e8c24d99ccd1d52a1cf9e84b8a32b8fdce2ae
semcmahon05 <semcmahon@wm.edu>
2026-09-02 14:47:32 -0400

    update

---
+++ API_Assignment.py
@@ -0,0 +1,20 @@
+# Team 6
+# Michael Garbarino, Nick Yennerell, Sadie McMahon
+
+
+# Dash App
+## Fetch API
+### 2 series over 1 year
+## Display a static image (logo, graphic, image)
+## Interactive Chart (line, area, bar, combo)
+### Hover, zoom, legend toggle
+### Callback: series to show, date range, transformation
+### Two analytics: normalization, percent change, rolling average, resampling
+# Labels and Source
+
+# Documentation
+## Question/Story
+## Data choices
+## Interactivity
+## Takeaway
+## Group Evaluation
